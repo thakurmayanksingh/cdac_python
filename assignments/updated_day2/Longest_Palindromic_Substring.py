eb8aa3212a645b307c74eb37a698a39dabe14f41
Mayank Singh <mst29310@gmail.com>
2026-08-29 21:48:34 +0530

    Updated Name

---
+++ assignments/updated_day2/Longest_Palindromic_Substring.py
@@ -0,0 +1,26 @@
+"""
+Exercise 9: Longest Palindromic Substring
+Write a program that prompts the user to enter a text string and finds the longest substring within it that reads the same forward and backward. If there are multiple palindromic substrings of the same maximum length, print any one of them.
+               01234
+Sample Input: "babad"
+Sample Output: "bab" (or "aba")
+Sample Input: "cbbd"
+Sample Output: "bb"
+"""
+
+def palindrome(st):
+    return st == st[::-1]
+
+def main():
+    inp = input("Sample Input: ")
+    max_len = 0
+    ans = ''
+    for i in range(len(inp)):
+        for j in range(i+1, len(inp)):
+            if palindrome(inp[i:j+1]):
+                max_len = max(max_len, len(inp[i:j+1]))
+                ans = inp[i:j+1]
+    print(f"Sample Output: {ans}")
+
+if __name__ == "__main__":
+    main()
